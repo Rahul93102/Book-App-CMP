2743bfe0825fba278777c40eb59b1e48ca0c2787
Rahul Jha <132360698+Rahul93102@users.noreply.github.com>
2025-08-22 23:52:11 +0530

    fix updation of databases

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/MainActivity.kt
@@ -14,18 +14,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-
-actual class DatabaseFactory(
-    private val context: Context
-) {
-    actual fun create(): RoomDatabase.Builder<FavoriteBookDatabase> {
-        val appContext = context.applicationContext
-        val dbFile = appContext.getDatabasePath(FavoriteBookDatabase.DB_NAME)
-
-        return Room.databaseBuilder(
-            context = appContext,
-            name = dbFile.absolutePath
-        )
-    }
-}
