1487ac6d13723a75f0ad3a81f5052f5caf630dd6
Rahul Jha <132360698+Rahul93102@users.noreply.github.com>
2025-08-22 23:55:37 +0530

    Remove database function common main 2

--- composeApp/src/desktopMain/kotlin/com/plcoding/bookpedia/di/Modules.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/plcoding/bookpedia/di/Modules.desktop.kt
@@ -6,25 +6,6 @@ import io.ktor.client.engine.okhttp.OkHttp
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
-actual class DatabaseFactory {
-    actual fun create(): RoomDatabase.Builder<FavoriteBookDatabase> {
-        val dbFile = documentDirectory() + "/${FavoriteBookDatabase.DB_NAME}"
-        return Room.databaseBuilder<FavoriteBookDatabase>(
-            name = dbFile
-        )
-    }
-
-    private fun documentDirectory(): String {
-        val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
-            directory = NSDocumentDirectory,
-            inDomain = NSUserDomainMask,
-            appropriateForURL = null,
-            create = false,
-            error = null
-        )
-        return requireNotNull(documentDirectory?.path)
-    }
-}
 
 
 actual val platformModule: Module
