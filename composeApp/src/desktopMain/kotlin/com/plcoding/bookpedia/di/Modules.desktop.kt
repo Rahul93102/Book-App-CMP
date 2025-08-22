3b1cf8c4ac12daaf0d9dddb167eeab7d7fb7e654
Rahul Jha <132360698+Rahul93102@users.noreply.github.com>
2025-08-22 23:55:19 +0530

    Remove database function common main

--- composeApp/src/desktopMain/kotlin/com/plcoding/bookpedia/di/Modules.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/plcoding/bookpedia/di/Modules.desktop.kt
@@ -8,22 +8,24 @@ import org.koin.dsl.module
 
 actual class DatabaseFactory {
     actual fun create(): RoomDatabase.Builder<FavoriteBookDatabase> {
-        val os = System.getProperty("os.name").lowercase()
-        val userHome = System.getProperty("user.home")
-        val appDataDir = when {
-            os.contains("win") -> File(System.getenv("APPDATA"), "Bookpedia")
-            os.contains("mac") -> File(userHome, "Library/Application Support/Bookpedia")
-            else -> File(userHome, ".local/share/Bookpedia")
-        }
+        val dbFile = documentDirectory() + "/${FavoriteBookDatabase.DB_NAME}"
+        return Room.databaseBuilder<FavoriteBookDatabase>(
+            name = dbFile
+        )
+    }
 
-        if(!appDataDir.exists()) {
-            appDataDir.mkdirs()
-        }
-
-        val dbFile = File(appDataDir, FavoriteBookDatabase.DB_NAME)
-        return Room.databaseBuilder(dbFile.absolutePath)
+    private fun documentDirectory(): String {
+        val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
+            directory = NSDocumentDirectory,
+            inDomain = NSUserDomainMask,
+            appropriateForURL = null,
+            create = false,
+            error = null
+        )
+        return requireNotNull(documentDirectory?.path)
     }
 }
+
 
 actual val platformModule: Module
     get() = module {
