0dbaab9e658d107a1a523ba46959d727a42daa52
Rahul Jha <132360698+Rahul93102@users.noreply.github.com>
2025-08-22 23:54:21 +0530

    Common Main database setup

--- composeApp/src/desktopMain/kotlin/com/plcoding/bookpedia/di/Modules.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/plcoding/bookpedia/di/Modules.desktop.kt
@@ -6,8 +6,29 @@ import io.ktor.client.engine.okhttp.OkHttp
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
+actual class DatabaseFactory {
+    actual fun create(): RoomDatabase.Builder<FavoriteBookDatabase> {
+        val os = System.getProperty("os.name").lowercase()
+        val userHome = System.getProperty("user.home")
+        val appDataDir = when {
+            os.contains("win") -> File(System.getenv("APPDATA"), "Bookpedia")
+            os.contains("mac") -> File(userHome, "Library/Application Support/Bookpedia")
+            else -> File(userHome, ".local/share/Bookpedia")
+        }
+
+        if(!appDataDir.exists()) {
+            appDataDir.mkdirs()
+        }
+
+        val dbFile = File(appDataDir, FavoriteBookDatabase.DB_NAME)
+        return Room.databaseBuilder(dbFile.absolutePath)
+    }
+}
+
 actual val platformModule: Module
     get() = module {
         single<HttpClientEngine> { OkHttp.create() }
         single { DatabaseFactory() }
     }
+
+
